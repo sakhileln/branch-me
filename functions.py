f05cadd0b2d2a96814eca467403e91c3bb50a041
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-02-11 00:10:59 +0200

    Add function to be implemented.

---
+++ functions.py
@@ -0,0 +1,11 @@
+def add(a: int, b: int) -> int:
+    """Returns the sum of two numbers."""
+    pass  # To be implemented
+
+def reverse_string(s: str) -> str:
+    """Returns the reverse of the given string."""
+    pass  # To be implemented
+
+def is_even(n: int) -> bool:
+    """Returns True if the number is even, otherwise False."""
+    pass  # To be implemented
